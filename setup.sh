a3e8e8655d502c84da8ced49cc6c940bcaf46d07
phillip-toohill <phillip.toohill@rackspace.com>
2023-05-16 16:41:43 -0500

    Updating setup and workflow

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ pwd
 # Clean the build dir which is base for all ospc projects..
 make clean -C ./docs-cloud-files/api-docs
 # Build the individual ospc sphinx projects
-make clean html -C ./docs-cloud-files/api-docs
-make clean html -C ./docs-cloud-load-balancers/api-docs/cloud-load-balancers-v1
-# Copy the landing page to the build dir 
+make html -C ./docs-cloud-files/api-docs
+make html -C ./docs-cloud-load-balancers/api-docs/cloud-load-balancers-v1
+# Copy the landing page to the build dir
 cp index.html ospc/docs
 :
